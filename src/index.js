7723472d7925bf88ed94f6374e8a84e3301b9f27
Diggers33 <cdigby@iris-eng.com>
2025-06-12 19:28:38 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ResourcePlanner from './ResourcePlanner';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ResourcePlanner />
+  </React.StrictMode>
+);
