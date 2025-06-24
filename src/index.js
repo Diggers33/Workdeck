a84090bd8edb33a284ac911c7d867faea98a690b
Diggers33 <cdigby@iris-eng.com>
2025-06-24 23:55:29 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ResourcePlanner from './ResourcePlanner';
+import './index.css';
+import WorkdeckApp from './WorkdeckApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ResourcePlanner />
+    <WorkdeckApp />
   </React.StrictMode>
 );
