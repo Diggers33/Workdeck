0198037492742fc4f7009683cd219d181503f507
Diggers33 <cdigby@iris-eng.com>
2025-07-14 15:46:10 +0200

    Update WorkdeckApp.js

--- src/WorkdeckApp.js
+++ src/WorkdeckApp.js
@@ -1,27 +1,8 @@
 import React from 'react';
+import ResourcePlanner from './ResourcePlanner';
 
 const WorkdeckApp = () => {
-  return (
-    <div style={{ 
-      minHeight: '100vh', 
-      display: 'flex', 
-      alignItems: 'center', 
-      justifyContent: 'center',
-      backgroundColor: '#ff0000',  // Bright red background
-      color: 'white',
-      fontFamily: 'Arial, sans-serif'
-    }}>
-      <div style={{ textAlign: 'center', padding: '2rem' }}>
-        <h1 style={{ fontSize: '3rem' }}>
-          🔥 TESTING 123 🔥
-        </h1>
-        <p style={{ fontSize: '2rem' }}>
-          Current time: {new Date().toLocaleTimeString()}
-        </p>
-        <p>If you see this red screen, the update worked!</p>
-      </div>
-    </div>
-  );
+  return <ResourcePlanner />;
 };
 
 export default WorkdeckApp;
